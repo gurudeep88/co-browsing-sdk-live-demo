7725891b620b3609cec89d5b6e90e155d90281b0
gurudeep88 <gurudeep88@gmail.com>
2023-08-23 23:14:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Button } from '@mui/material';
 import './App.css';
 import MuiTable from './components/MuiTable';
+import { coBrowsing } from 'sariska-cobrowsing';
 //import Sample from './components/Sample';
 
 function App() {
+  const startCobrowsing = async()=>{
+    await coBrowsing.startCoBrowsing('guru', 'cobrowsing-session');
+  }
+  const stopCobrowsing = async()=>{
+    await coBrowsing.stopCoBrowsing();
+  }
   return (
     <div className="App">
+      <Button onClick={startCobrowsing}>Start Cobrowsing</Button>
+      <Button onClick={stopCobrowsing}>Stop Cobrowsing</Button>
       <MuiTable />
       {/* <Sample /> */}
     </div>
